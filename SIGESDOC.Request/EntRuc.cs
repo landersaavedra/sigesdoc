e018110984c6c18a4dfa252a1cb3bae7e99b9934
lander saavedra <landersaavedra@gmail.com>
2019-11-15 09:52:07 -0500

    Subiendo load para carga de reniec y sunat

---
+++ SIGESDOC.Request/EntRuc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIGESDOC.Request
+{
+    public class EntRuc
+    {
+        public string ddp_nombre { get; set; }
+        public string ddp_nomzon { get; set; }
+        public string ddp_refer1 { get; set; }
+        public string domicilioLegal { get; set; }
+        public string desc_dep { get; set; }
+        public string desc_prov { get; set; }
+        public string desc_dist { get; set; }
+
+    }
+}
---
+++ SIGESDOC.Web/Controllers/MesadePartesVirtual/Despacho/DespachoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIGESDOC.Web.Controllers.MesadePartesVirtual.Despacho
+{
+    public class DespachoController : Controller
+    {
+        // GET: Despacho
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
